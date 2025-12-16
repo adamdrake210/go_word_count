0676bc2baa9f81cda92a6d3679fbae23dac5255f
Adam Drake <adam@chemify.io>
2025-12-16 14:27:50 +0100

    updates

--- main.go
+++ main.go
@@ -8,14 +8,44 @@ import (
 )
 
 func main() {
-	data, err := os.ReadFile("./wors.txt")
+	filename := "./words.txt"
+
+	log.SetFlags(0)
+
+	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatalln("failed to read file:", err)
 	}
 
-	wordCount := CountWords(data)
+	wordCount := CountWordsInFile(file)
 
 	fmt.Println(wordCount)
+}
+
+func CountWordsInFile(file *os.File) int {
+	countWords := 0
+	isInsideWord := false
+
+	const bufferSize = 7
+	buffer := make([]byte, bufferSize)
+
+	for {
+		size, err := file.Read(buffer)
+		if err != nil {
+			break
+		}
+
+		isInsideWord = !unicode.IsSpace(rune(buffer[0])) && isInsideWord
+
+		bufferCount := CountWords(buffer[:size])
+		if isInsideWord {
+			bufferCount -= 1
+		}
+		countWords += bufferCount
+
+		isInsideWord = !unicode.IsSpace(rune(buffer[size-1]))
+	}
+	return countWords
 }
 
 func CountWords(data []byte) int {
