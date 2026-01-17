cb9528b24128f73f2e6fe51ac2b0d6c4d8a2af56
Adam Drake <adamgedrake@gmail.com>
2026-01-17 14:25:01 +0100

    added in cli flags and display options (#4)
    Co-authored-by: Adam Drake <adam@chemify.io>

--- main.go
+++ main.go
@@ -1,19 +1,61 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
 )
 
+type DisplayOptions struct {
+	ShowBytes bool
+	ShowWords bool
+	ShowLines bool
+}
+
+func (d DisplayOptions) ShouldShowBytes() bool {
+	if !d.ShowBytes && !d.ShowWords && !d.ShowLines {
+		return true
+	}
+
+	return d.ShowBytes
+}
+
+func (d DisplayOptions) ShouldShowLines() bool {
+	if !d.ShowBytes && !d.ShowWords && !d.ShowLines {
+		return true
+	}
+
+	return d.ShowLines
+}
+
+func (d DisplayOptions) ShouldShowWords() bool {
+	if !d.ShowBytes && !d.ShowWords && !d.ShowLines {
+		return true
+	}
+
+	return d.ShowWords
+}
+
 func main() {
+	opts := DisplayOptions{}
+
+	flag.BoolVar(&opts.ShowWords, "w", false, "Used to toggle whether or not to show the word count")
+
+	flag.BoolVar(&opts.ShowLines, "l", false, "Used to toggle whether or not to show the lines count")
+
+	flag.BoolVar(&opts.ShowBytes, "c", false, "Used to toggle whether or not to show the byte count")
+
+	flag.Parse()
+
 	log.SetFlags(0)
 
 	totals := Counts{}
-	filenames := os.Args[1:]
+
+	filenames := flag.Args()
 	didError := false
 
-	for _, filename := range os.Args[1:] {
+	for _, filename := range filenames {
 		counts, err := CountFile(filename)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "counter:", err)
@@ -23,15 +65,15 @@ func main() {
 
 		totals = totals.Add(counts)
 
-		counts.Print(os.Stdout, filename)
+		counts.Print(os.Stdout, opts, filename)
 	}
 
 	if len(filenames) == 0 {
-		GetCounts(os.Stdin).Print(os.Stdout)
+		GetCounts(os.Stdin).Print(os.Stdout, opts)
 	}
 
 	if len(filenames) > 1 {
-		totals.Print(os.Stdout, "total")
+		totals.Print(os.Stdout, opts, "total")
 	}
 
 	if didError {
